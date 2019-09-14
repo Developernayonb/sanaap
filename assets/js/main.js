43c738b5b652dcfe4e618d03f08715285c7faef8
mdnayonbabujibon <49094460+mdnayonbabujibon@users.noreply.github.com>
2019-09-15 03:29:59 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+	// Owl Carousel Active
+
+    $('.home-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
